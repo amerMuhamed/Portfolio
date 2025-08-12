a48e5ba784a1977f018708a7e9841004fe969380
amer <amermuhamedamer@gmail.com>
2025-08-12 13:47:38 +0300

    add section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export default function Navbar() {
     });
 
     return () => observer.disconnect();
-  }, []);
+  }, [sections]);
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-black/30 backdrop-blur-md shadow-md z-50">
